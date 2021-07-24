69bb415a10b54c6218bd5e55b09fc17ce8947b0d
Fahad-D-Riano <71026825+Fahad-D-Riano@users.noreply.github.com>
2021-07-24 22:06:12 +0400

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,40 @@ from dotenv import load_dotenv
 
 project_folder = os.path.expanduser("~/ToDo")
 load_dotenv(os.path.join(project_folder, ".env"))
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", default="secret")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = "todo"
+
+
+class User (UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), index=True, unique=True)
+    email = db.Column(db.String(255), index=True, unique=True)
+    password_hash = db.Column(db.String(200))
+    todo_items = db.relationship("ToDo", backref="author", lazy="dynamic")
+    tag_items = db.relationship("Tags", backref="author", lazy="dynamic")
+
+
+class ToDo (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), index=True)
+    tag = db.Column(db.String(100), index=True)
+    body = db.Column(db.Text)
+    start_date = db.Column(db.DateTime)
+    due_date = db.Column(db.DateTime, index=True)
+    completed = db.Column(db.Boolean, default=False, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
+class Tags (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
